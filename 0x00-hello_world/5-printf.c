650bc14cfb62b55f36b4a8ebcbdaaa02dee16f5a
Muluken Kebede <mulerami9@gmail.com>
2022-06-02 15:33:35 -0700

    size of

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#inlude<stdio.c>
+
+/*main vpid*/
+int main(void)
+{
+	printf("with grammar, but theoutcome is a pice og art\n");
+
+	return(0);			
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#inlude<stdio.h>
+/*main function*/
+int main(void)
+{
+	char acharter;
+	int ainteger;
+	long along;
+	long long alogalong;
+	float afloat;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
+	printf("Size of an int: %lu byte(S)\n", sizeof(ainteger));
+	printf("Size of long int: %lu byte(s)\n", sizeof(along));
+	printf("Size of a long long int: %lu byte(s)\n",sizeof (alonglong));
+	printf("Size ofa float: %lu byte (s)\n", sizeof(afloat);
+
+	Return 0;
+
+
+}   
