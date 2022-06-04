49dde4d28a2044b618542a83b55e5e851f22465e
Muluken Kebede <mulerami9@gmail.com>
2022-06-03 18:47:15 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+
+for (char i = 'a'; i <= 'z'; i++)
+{
+	putchar(i);
+}
+for (char i = 'A'; i <= 'Z'; i++)
+{
+	putchar(i);
+}
+putchar('\n');
+return (0);
+}
