7dc011fd256d2e84b223d07f6376bbaec0a2d6fc
Muluken Kebede <mulerami9@gmail.com>
2022-06-05 12:46:18 -0700

    print comb

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	int hundreds;
+	int tens;
+	int ones;
+
+	for (hundreds = 0; hundreds <= 9; hundreds++)
+	{
+		for (tens = hundreds + 1; tens <= 9; tens++)
+		{
+			for (ones = tens + 1; ones <= 9; ones++)
+			{
+				putchar(hundreds + '0');
+				putchar(tens + '0');
+				putchar(ones + '0');
+
+				if (tens < 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
