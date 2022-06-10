ecfeaf271a060f1cc19400050a64e79c97881f29
Muluken Kebede <mulerami9@gmail.com>
2022-06-10 13:18:24 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,13 @@
+#include <stdio.h>
 #include "main.h"
-/*
+/**
  * main -Entry point
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	puts("_putchar");
 
 	return (0);
 }
