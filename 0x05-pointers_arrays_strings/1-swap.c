d6c1cc7d3596f61a3d0240b8f5d56f137a820fe0
Muluken Kebede <mulerami9@gmail.com>
2022-06-13 13:41:32 -0700

    swap int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * swap_int - swaps values
+ * @a: integer value of num 1
+ * @b: integer value of num 2
+ * return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	
+	*a = *b;
+	*b = c;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
-void _putchar (char c);
 void reset_to_98(int *n);
 
 #endif
