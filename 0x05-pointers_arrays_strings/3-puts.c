c7d93ccbc01f8373eb3417f350ac04bc9a0b28c6
Muluken Kebede <mulerami9@gmail.com>
2022-06-13 23:27:25 -0700

    main.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ *_puts - prints a string, to stdout
+ *@str: value to be evaluate.
+ *Return: void
+ */
+void _puts(char *str)
+{
+
+	int c = 0;
+
+	while (c >= 0)
+	{
+		if (str[c] == '\0')
+		{
+			_puthar('\n');
+			break;
+		}
+		_putchar(str[c]);
+		c++;
+	}
+}
