ae40b21ebb6538f8b3928e5e26970222697c29a6
Muluken Kebede <mulerami9@gmail.com>
2022-06-29 13:22:02 -0700

    Static libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,74 +1,21 @@
-
-
 #include "main.h"
-
-
-
 #include <unistd.h>
-
-
-
 /**
- *
- *
- *
- *  *_puts - prints a string, to stdout
- *
- *
- *
- *   *@str: value to be evaluate.
- *
- *
- *
- *    *Return: void
- *
- *
- *
- *     */
-
-
-
+ *_puts - prints a string, to stdout
+ *@str: value to be evaluate.
+ *Return: void
+ */
 void _puts(char *str)
-
-
-
 {
-
-
-
-		int c = 0;
-
-
-
-
-
-
-
-			while (c >= 0)
-
-
-
-					{
-
-
-
-								if (str[c] == '\0')
-
-
-
-											{
-
-
-
-															_putchar('\n');
-
-
-
-																		break;
-
-
-
-																				}
+	int c = 0;
+	
+	while (c >= 0)
+	{
+		if (str[c] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
 
 
 
