6e41af4340d32e03bca08cc282e98c126296b83d
Muluken Kebede <mulerami9@gmail.com>
2022-06-29 13:07:42 -0700

    Static libraries

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,1 +1,73 @@
-int _abs(int n);
+
+
+ 
+
+ 
+
+
+
+#include "main.h"
+
+
+
+/**
+ *
+ *
+ *
+ *  *_abs - gives the absolute value of an integer.
+ *
+ *
+ *
+ *   *@n: input number as an integer.
+ *
+ *
+ *
+ *    *Return: absolute value
+ *
+ *
+ *
+ *     */
+
+
+
+int _abs(int n)
+
+
+
+{
+
+
+
+		if (n >= 0)
+
+
+
+				{
+
+
+
+							return (n);
+
+
+
+								}
+
+
+
+			else
+
+
+
+					{
+
+
+
+								return (n * -1);
+
+
+
+									}
+
+
+
+}
