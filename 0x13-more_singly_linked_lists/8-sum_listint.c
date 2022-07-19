13af5429a4ce659f7a32c33fd6fff44ca0238091
Muluken Kebede <mulerami9@gmail.com>
2022-07-19 14:21:58 -0700

    sum listint

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -4,13 +4,13 @@
  * a linked list.
  * @head: head of a list.
  *
- * Return: sum of all the data (n).
+ * Return: sum of all the datasum += head->n, head = head->next)
  */
 int sum_listint(listint_t *head)
 {
 	int sum;
 
-	for (sum += head->n, head = head->next)
+	for (sum = 0; head; sum += head->n, head = head->next)
 		;
 	return (sum);
 }
