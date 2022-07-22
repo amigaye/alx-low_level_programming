7c08ceab53f23940d5ae710015ae756e25a693a8
Muluken Kebede <mulerami9@gmail.com>
2022-07-22 00:04:46 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * main -Entry point
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
-	puts("_putchar");
+	int i;
+	char p[] = "_putchar";
 
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(p[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
