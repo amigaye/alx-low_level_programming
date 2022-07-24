c9fc2aae2809b5ed73baf1400da31564090a6025
Muluken Kebede <mulerami9@gmail.com>
2022-07-24 12:54:37 -0700

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * times_table - print time table
+ *
+ * Return: time table
+ */
+void times_table(void)
+{
+	int a, b, op;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(0 + '0');
+		for (b = 1; b <= 9; b++)
+		{
+			op = a * b;
+			_putchar(',');
+			_putchar(' ');
+			if (op <= 9)
+			{
+				_putchar(' ');
+				_putchar(op + 48);
+			}
+			else
+			{
+				_putchar((op / 10) + 48);
+				_putchar((op % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
