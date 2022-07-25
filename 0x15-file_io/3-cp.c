7ea2a4505788088df16ec60077d435c7af4882b5
Muluken Kebede <mulerami9@gmail.com>
2022-07-25 15:32:54 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,12 +7,13 @@
  * @argv: arguments vector.
  * Return: no return.
  */
-void error_file(nt file_from, int file_to, char *argv[])
+void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
+	}
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
