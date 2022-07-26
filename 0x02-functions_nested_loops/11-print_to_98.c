58e3d1d93f338eab4c52d6e705762a4800f00ed8
Muluken Kebede <mulerami9@gmail.com>
2022-07-26 00:58:26 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,41 +18,26 @@ void print_to_98(int n)
 				_putchar(-(i / 100) + '0');
 				_putchar(-((i % 100) / 10) + '0');
 				_putchar(-((i % 100) % 10) + '0');
-				if (i != 98)
-					_putchar(',');
-				_putchar(' ');
 			}
 			else if (i < -9)
 			{
 				_putchar('-');
 				_putchar(-(i / 10) + '0');
 				_putchar(-(i % 10) + '0');
-				if (i != 98)
-					_putchar(',');
-				_putchar(' ');
 			}
 			else if (i < 0)
 			{
 				_putchar('-');
 				_putchar(-i + '0');
-				if (i != 98)
-					_putchar(',');
-				_putchar(' ');
 			}
 			else if (i <= 9)
 			{
 				_putchar(i + '0');
-				if (i != 98)
-					_putchar(',');
-				_putchar(' ');
 			}
 			else
 			{
 				_putchar(i / 10 + '0');
 				_putchar(i % 10 + '0');
-				if (i != 98)
-					_putchar(',');
-				_putchar(' ');
 			}
 		}
 	else
@@ -64,19 +49,18 @@ void print_to_98(int n)
 				_putchar(i / 100 + '0');
 				_putchar((i % 100) / 10 + '0');
 				_putchar((i % 100) % 10 + '0');
-				if (i != 98)
-					_putchar(',');
-				_putchar(' ');
 			}
 			else
 			{
 				_putchar(i / 10 + '0');
 				_putchar(i % 10 + '0');
-				if (i != 98)
-					_putchar(',');
-				_putchar(' ');
 			}
 		}
+	}
+	if (i != 98)
+	{
+		_putchar(',');
+		_putchar(' ');
 	}
 	_putchar('\n');
 }
