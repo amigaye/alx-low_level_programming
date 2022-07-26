f70342bcba4eac758ef61902e0381ff3d9a8d13d
Muluken Kebede <mulerami9@gmail.com>
2022-07-26 01:56:49 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,58 +9,26 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n <= 98)
+	if (n < 98)
+	{
 		for (i = n; i <= 98; i++)
 		{
-			if (i <= -100)
-			{
-				_putchar('-');
-				_putchar(-(i / 100) + '0');
-				_putchar(-((i % 100) / 10) + '0');
-				_putchar(-((i % 100) % 10) + '0');
-			}
-			else if (i < -9)
-			{
-				_putchar('-');
-				_putchar(-(i / 10) + '0');
-				_putchar(-(i % 10) + '0');
-			}
-			else if (i < 0)
-			{
-				_putchar('-');
-				_putchar(-i + '0');
-			}
-			else if (i <= 9)
-			{
-				_putchar(i + '0');
-			}
+			if (i != 98)
+				printf("%d%c%c", i, ',', ' ');
 			else
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-			}
+				printf("%d\n", i);
 		}
-	else
+	}
+	else if (n > 98)
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n; i >=  98; i--)
 		{
-			if (i >= 100)
-			{
-				_putchar(i / 100 + '0');
-				_putchar((i % 100) / 10 + '0');
-				_putchar((i % 100) % 10 + '0');
-			}
+			if (i != 98)
+				printf("%d%c%c", i, ',', ' ');
 			else
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-			}
+				printf("%d\n", i);
 		}
 	}
-	if (i != 98)
-	{
-		_putchar(',');
-		_putchar(' ');
-	}
-	_putchar('\n');
+	else
+		printf("%d\n", n);
 }
