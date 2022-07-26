b8e7854e0ed62b8379a105f2f19e3d1a730e9a97
Muluken Kebede <mulerami9@gmail.com>
2022-07-26 13:28:07 -0700

    sum of multiple 3 & 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - return multiple 3 and 5
+ * Return: print multiple of 3 & 5 below 1024
+ */
+int main (void)
+{
+	int i;
+	int sum;
+	int mult3;
+	int mult5;
+
+	sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+}
